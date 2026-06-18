4ea8023765663a519279453cb74e72b7af745077
tanvir <mstanvirislam05@gmail.com>
2026-06-18 06:15:17 +0600

    feat: implement API endpoint to fetch products from MongoDB

--- index.js
+++ index.js
@@ -32,9 +32,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const database = client.db("monkey")
-    const jobsCollection = database.collection("jobs");
+    const productsCollection = database.collection("products");
    
-
+    app.get('/api/products',async(req,res)=>{
+      const result=await productsCollection.find().toArray()
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
